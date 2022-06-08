0c5c361f5798bf2e1504613bdc6cd6a7c97fd2e1
Takuma Kawai <r9ag6fx0adr@gmail.com>
2022-06-08 22:28:59 +0900

    improve algorithm

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 import random
 import functools
@@ -218,10 +219,32 @@ def optimize(layout):
             break
     return layout
 
+def try_mirror(layout):
+    mirrored = np.rot90(layout.init_layout, 2)
+    layout2 = Layout((layout.init_layout + mirrored).clip(0, 1))
+    layout2.run()
+    if layout2.score > layout.score:
+        layout = layout2
+    return layout
+
+def print_result(layout):
+    print(layout.graph_both())
+    print(layout.summary())
+    print(layout.to_hex())
+
+def check(layout):
+    layout.run()
+    layout = optimize(layout)
+    layout = try_mirror(layout)
+    if layout.mana == 36000:
+        print_result(layout)
+        sys.exit(0)
+    return layout
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', metavar='S', type=int, default=4)
-    parser.add_argument('-k', metavar='K', type=int, default=6)
+    parser.add_argument('-s', metavar='S', type=int, default=8)
+    parser.add_argument('-k', metavar='K', type=int, default=14)
     parser.add_argument('--init', metavar='INIT_LAYOUT')
     args = parser.parse_args()
 
@@ -230,54 +253,26 @@ def main():
 
     random.seed()
 
-    layout = Layout.zeros()
+    init_layout = Layout.zeros()
+    init_layout.run()
 
     if args.init:
-        layout = Layout.from_hex(args.init)
+        init_layout = Layout.from_hex(args.init)
+        init_layout.run()
+        print(finit_layout.summary())
+        print(new_layout.to_hex())
 
-    layout.run()
-    # print(layout.graph_both())
-    print(f'gen0: {layout.summary()}')
-
+    best = init_layout
     try:
-        i = 1
         while True:
-            cand = []
-            while True:
-                new_layout = layout.evolve()
-                new_layout.run()
-                if new_layout.score > layout.score:
-                    new_layout = optimize(new_layout)
-                    cand.append(new_layout)
-
-                if layout.cost() == 0:
-                    if len(cand) >= 1000:
-                        break
-                else:
-                    if len(cand) >= 10:
-                        break
-            new_layout = sorted(cand, key=lambda l: l.score)[-1]
-            print(f'gen{i}: {new_layout.summary()}')
-            print(new_layout.to_hex())
-            layout = new_layout
-            i += 1
+            new_layout = check(init_layout.evolve())
+            if new_layout.score > best.score:
+                print(new_layout.summary())
+                print(new_layout.to_hex())
+                best = new_layout
     except KeyboardInterrupt:
         print('\r', end='')
-        pass
-
-    if layout.mana == 18000:
-        mirrored = np.rot90(layout.init_layout, 2)
-        layout2 = Layout(layout.init_layout + mirrored)
-        layout2.run()
-        print(layout2.graph_both())
-        print(layout2.summary())
-        if layout2.score > layout.score:
-            layout = layout2
-
-    layout.run()
-    print(layout.graph_both())
-    print(layout.summary())
-    print(layout.to_hex())
+        print_result(try_mirror(best))
 
 if __name__ == '__main__':
     main()
